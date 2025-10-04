f272ca8c6457d50dc1270ebb15a9493583bb9885
Michelle-Linnn <wlin13@inside.artcenter.edu>
2025-10-04 15:40:28 -0700

    Update script.js

--- WEEK3/inclass/script/script.js
+++ WEEK3/inclass/script/script.js
@@ -1,5 +1,27 @@
 let cbox = document.getElementById("colorBox");
+let colorBtn = document.getElementById("changecolor")
+let imgBox = document.getElementById("Rainboaimage")
+let ImageBtn =document.getElementById("toggleImage")
 
-console.log(cbox);
+let assignRandomColor = function()
+{
+    let rCopm = 255 * Math.random()
+    let gComp = 255 * Math.random()
+    let bComp = 255 * Math.random()
+    cbox.style.backgroundColor ="rgb("+ rCopm +","+gComp+","+bComp+")"
+}
 
-cbox.style.backgroundColor ="rgb(255,1,2)";
+const toggleRainboaImage = ()=>
+{
+    console.log(imgBox.src)
+    if(imgBox.src.includes("Rainboa"))
+    {
+        imgBox.src = "images/abc.png"
+    }
+    else
+    {
+        imgBox.src="images/Rainboa.webp"
+    }
+}
+ImageBtn.addEventListener("click", toggleRainboaImage)
+colorBtn.addEventListener("click", assignRandomColor)
