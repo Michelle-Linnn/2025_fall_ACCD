379e6dc5ecfe7cefc7fa471dd20b69d72a130a29
Michelle-Linnn <wlin13@inside.artcenter.edu>
2025-10-17 10:42:40 -0700

    Update sketch.js

--- WEEK5/inclass/sketch.js
+++ WEEK5/inclass/sketch.js
@@ -1,14 +1,26 @@
+
+let posX = [];
+let posY = [];
+let size = []
+let numStars = 500;
+
 function setup() {
   createCanvas(600, 600);
   colorMode(HSB,360,100,100);
-  frameRate(20);
+  //frameRate(20);
+  for(let i = 0;i < numStars; i++){
+    posX.push(random(width))
+    posY[i] = random(height)
+    size.push (random(2,10))
+  }
 }
 
 function draw() {
   background(0,0,0)
-  for(let i = 0;i < 2; i++){
-    fill(random(360),80,100)
-    circle(random(width),random(height),50);
+  fill(0,0,100)
+  for(let i = 0;i < numStars; i++){
+    //fill(random(360),80,100)
+    circle(posX[i],posY[i],random(size[i],size[i+1]));
   }
 
 }
