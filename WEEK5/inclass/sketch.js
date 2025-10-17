d1fd58f5088076aa875110e124c484e71dade302
Michelle-Linnn <wlin13@inside.artcenter.edu>
2025-10-17 09:52:11 -0700

    Create sketch.js

---
+++ WEEK5/inclass/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(600, 600);
+  colormade(HSB,360,100,100);
+}
+
+function draw() {
+  for(let i = 0;i < 8; i++)
+circle(random);
+}
+
+
+//while(){
+ // fill(random)
+//}
