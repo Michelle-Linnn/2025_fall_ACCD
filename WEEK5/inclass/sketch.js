49729893c4611f11703955881001f6c1aad82c19
Michelle-Linnn <wlin13@inside.artcenter.edu>
2025-10-17 10:09:40 -0700

    Update sketch.js

--- WEEK5/inclass/sketch.js
+++ WEEK5/inclass/sketch.js
@@ -1,15 +1,17 @@
 function setup() {
   createCanvas(600, 600);
-  colormade(HSB,360,100,100);
+  colorMode(HSB,360,100,100);
+  frameRate(20);
 }
 
 function draw() {
   background(0,0,0)
-  for(let i = 0;i < 8; i++)
+  for(let i = 0;i < 2; i++){
     fill(random(360),80,100)
-  circle(random(width),random(height),50);
-}
+    circle(random(width),random(height),50);
+  }
 
+}
 
 //while(){
  // fill(random)
